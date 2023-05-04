87664fcd4710fa9b0d1b6c39de09821aa8cb0c4d
Algebra-FUN <algebra-fun@outlook.com>
2023-05-04 11:44:37 +0800

    [add] scrape script

---
+++ code/MTRStationScraper.py
@@ -0,0 +1,50 @@
+import requests as req
+from bs4 import BeautifulSoup
+import json
+import re
+import geopandas as gpd
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+def dms_to_decimal(dms):
+    deg, minutes, seconds, direction =  re.split('[^A-Za-z0-9]', dms)
+    decimal_degrees = float(deg) + float(minutes)/60 + float(seconds)/(60*60)
+    if direction in ['W', 'S']:
+        decimal_degrees *= -1
+    return decimal_degrees
+
+def get_station_list():
+    index_url = "https://en.wikipedia.org/wiki/List_of_MTR_stations"
+    response = req.get(index_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    a_list = soup.select("#mw-content-text > div.mw-parser-output > table > tbody > tr > td:nth-child(2) > a")
+    station_list = [a.attrs for a in a_list]
+    return station_list
+
+def find_station_longlat(url):
+    response = req.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    lat = soup.select_one("span.latitude").text
+    long = soup.select_one("span.longitude").text
+    return {"lat": lat, "long": long}
+
+def get_stations_info():
+    station_list = get_station_list()
+    for station in station_list:
+        url = f"https://en.wikipedia.org{station['href']}"
+        print("get:",url)
+        latlong = find_station_longlat(url)
+        station.update(latlong)
+    return station_list
+
+def preprocess(station_list):
+    for station in station_list:
+        try:
+            station["lat"] = dms_to_decimal(station["lat"])
+            station["long"] = dms_to_decimal(station["long"])
+        except:
+            pass
+        station["title"] = station["title"].replace("(MTR)", "").replace("station", "").strip()
+    return station_list
--- data/README.md
+++ data/README.md
@@ -1,5 +1,39 @@
-# Intro for Data
+# Datasets
+
+## Notice
 
 Since there is no free lunch, we only provide the public dataset for this open source.
 
 The Customer's Rating Data used in GeoCommend Model is scraped from the internet with our web-crawler script, so you won't get it for free here, unless you want pay us for it.
+
+## Description
+
+### Location Data for selected fast food brands
+
+| Brand | File |
+|----------|-----------|
+| Yoshinoya | [yoshinoya.json](yoshinoya.json) |
+| McDonald's | [mcdonalds.json](mcdonalds.json) |
+| KFC | [kfc.json](kfc.json) |
+| Fairwood | [fairwood.json](fairwood.json) |
+| Cafe de Coral | [cdc.json](cdc.json) |
+
+These datasets record the latitude and longitude of each store of the brand. Some of them also record type of the store.
+
+### Demographic Public Dataset
+
+These datasets also from CSDI, thanks to them for providing these data for free. The description of each dataset can be found in the CSDI website.
+
+| Dataset | File |
+|----------|-----------|
+| Public Market in HK | [PMRK.geojson](PMRK.geojson) |
+| Demographic Data in HK | [DCCA_21C.geojson](DCCA_21C.geojson) |
+| Rental Data in HK | [Shop_and_Other_Commercial_Assessments.geojson](Shop_and_Other_Commercial_Assessments.geojson) |
+
+### MTR Station Data
+
+[MTRStations.json](MTRStations.json)
+
+which includes the latitude and longitude of each MTR station in Hong Kong.
+
+We also provide a script to scrape the data from the Wiki, which is [`MTRStationScraper.py`](../code/MTRStationScraper.py).
